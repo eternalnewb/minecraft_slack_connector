71ef96c2dc0c9af84f5c9c270c715448d69b72ce
Jerome <knappjt@gmail.com>
2015-02-12 00:12:19 -0600

    clean up arg parse, add config file function, clean -and break- curl

--- connect.sh
+++ connect.sh
@@ -8,14 +8,11 @@ verbose=0
 show_help() {
 cat << EOF
 
-Usage: ${0##*/} [-h] -l /path/to/server.log -u slackWebhookURL | -f path to config file 
+Usage: ${0##*/} [-h] | -f path to config file 
 -l and -u will override the config file
 
 	-h		Display this help and exit
-	-l | --logfile 	Path to your minecraft server.log file. Assumed
-			to be ../server.log if unsupplied
-	-u | --URL	URL notifications will be posted to
-	-f		path to config file holding -l and -u's data
+	-f		path to config file
 
 EOF
 }
@@ -23,11 +20,11 @@ EOF
 # Argument Parsing
 while [ "$#" -gt 0 ]; do
     case $1 in
-        -h|-\?|--help)   # Call a "show_help" function to display a synopsis, then exit.
+        -h|-\?|--help)
             show_help
             exit
             ;;
-        -f|--file)       # Takes an option argument, ensuring it has been specified.
+        -f|--file)   
             if [ "$#" -gt 1 ]; then
                 file=$2
                 shift 2
@@ -44,13 +41,6 @@ while [ "$#" -gt 0 ]; do
             echo 'ERROR: Must specify a non-empty "--file FILE" argument.' >&2
             exit 1
             ;;
-        -v|--verbose)
-            verbose=$((verbose + 1)) # Each -v argument adds 1 to verbosity.
-            ;;
-        --)              # End of all options.
-            shift
-            break
-            ;;
         -?*)
             printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
             ;;
@@ -58,10 +48,10 @@ while [ "$#" -gt 0 ]; do
             break
     esac
 
-    shift
+    shift #increment the argument number??!
 done
 
-# Suppose --file is a required option. Check that it has been set.
+# --file is a required option. Check that it has been set.
 if [ ! "$file" ]; then
     echo 'ERROR: option "--file FILE" not given. See --help.' >&2
     exit 1
@@ -71,7 +61,6 @@ fi
 
 #Push Minecraft Joins & Parts to Slack via webhook API
 
-WEBHOOK_URL=''
 
 # Another way to ctrl-c out of the while loop because idk what im doing
 cleanup ()
@@ -81,18 +70,35 @@ exit 0
 }
 trap cleanup SIGINT SIGTERM
 
+# Read config
+source $file
+# Provides: 
+# $WEBHOOK_URL
+# $SLACK_CHANNEL
+# $BOT_NAME
+# $ICON_EMOJI
+# $MINECRAFT_SERVER_LOG_FILE
 
 while true; do
   # Watcher.py is a way to avoid futzing with inotify.
   # https://bitbucket.org/denilsonsa/small_scripts/src/b20d762b9c1a0d250ddbd8e26850df62d84b1559/sleep_until_modified.py?at=default
-  # server.log is your minecraft server.log file, will work in vanilla & most modpacks
 
-  ./watcher.py /opt/tekkit/server.log
-  MSG=`tail -n2 server.log | grep 'left\|joined\|to \|was \|blew\|died\|fell\|ground\|drowned\|slain\|flames\|lava\|wall' | sed 's/^.*] //'`;
+  ./watcher.py MINECRAFT_SERVER_LOG_FILE
+  MSG=`tail -n2 MINECRAFT_SERVER_LOG_FILE | grep 'left\|joined\|to \|was \|blew\|died\|fell\|ground\|drowned\|slain\|flames\|lava\|wall' | sed 's/^.*] //'`;
   echo $MSG;
 
+	#todo turn this into a variable in the script instead of a file
   if [[ $MSG == $(cat last_slack) ]]; then echo "Duplicate message detected: $MSG"; continue; fi
 
-  curl -X POST --data-urlencode "payload={\"channel\": \"#minecraft\", \"username\": \"minecraft.whatever.com:443\", \"text\": \"$MSG\", \"icon_emoji\": \":tekkit:\"}" $WEBHOOK_URL;
-  echo "$MSG" > last_slack
+  THIS_HOOK='"payload={"channel": "'$SLACK_CHANNEL'", "username": "'$BOT_NAME\
+  '", "text": "'$MSG'", "icon_emoji": "'$ICON_EMOJI'"}"';
+  
+  curl -X POST --data-urlencode "$THIS_HOOK" "$WEBHOOK_URL"
+
+#   THIS_HOOK="$THIS_HOOK$SLACK_CHANNEL"
+#   THIS_HOOK='$THIS_HOOK", "username": "'
+#   THIS_HOOK="$THIS_HOOK$BOT_NAME"
+#   THIS_HOOK='", "text": "'
+#   curl -X POST --data-urlencode "payload={\"channel\": \"#minecraft\", \"username\": \"minecraft.whatever.com:443\", \"text\": \"$MSG\", \"icon_emoji\": \":tekkit:\"}" $WEBHOOK_URL;
+   echo "$MSG" > last_slack
 
